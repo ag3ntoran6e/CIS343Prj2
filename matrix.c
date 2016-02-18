97125bae98539182355da2424a4b0d899ee1aefc
Morrie Cunningham <cunningm@mail.gvsu.edu>
2016-02-18 09:59:50 -0500

    First Commit!
    Added the 3 main files we will need to use. Also, attached the project
    specifications.

---
+++ matrix.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "matrix.h"
+
+/****************************************************************************
+ * Creates and returns a pointer to a matrix object with the specified		*
+ * number of rows and columns. The "data" field is set to a dynamically 	*
+ * created array of ints of size rows * columns.							*
+ *																			*
+ * If the value of rows or columns is zero or negative, return NULL.		*
+ ***************************************************************************/
+Matrix *create(int rows, int columns)
+{
+	Matrix *result = NULL;
+	
+	// TO DO
+
+	return result;
+}
+
+/****************************************************************************
+ * Returns the matrix element at (row,column). Return INT_MIN (limits.h)	*
+ * if either row and/or column is invalid. Row and column values start at 	*
+ * zero. DO NOT modify the input matrix.									*
+ ***************************************************************************/
+int getValueAt(Matrix *m, int row, int column)
+{
+	// TO DO
+	
+	return INT_MIN;
+}
+
+/****************************************************************************
+ * If the row and column values are valid, sets the matrix element at 		*
+ * (row,column) with the parameter value. Row and column values start at	*
+ * zero.																	*
+ ***************************************************************************/
+void setValueAt(Matrix *m, int row, int column, int value)
+{
+	// TO DO
+}
+
+/****************************************************************************
+ * If the input matrices are compatible, then performs matrix addition and	*
+ * returns a pointer to the result matrix.									*
+ * Use create(), getValueAt(), and setValueAt() functions to implement this	*
+ * function. If the input matrices are not compatible, return NULL.			*
+ * DO NOT modify the input matrices.										*
+ ***************************************************************************/
+Matrix *add(Matrix *m1, Matrix *m2)
+{
+	Matrix *result = NULL;
+	
+	// TO DO
+
+	return result;
+}
+
+/****************************************************************************
+ * If the input matrices are compatible, then performs matrix subtraction	*
+ * and returns a pointer to the result matrix.								*
+ * Use create(), getValueAt(), and setValueAt() functions to implement this	*
+ * function. If the input matrices are not compatible, return NULL.			*
+ * DO NOT modify the input matrices.										*
+ ***************************************************************************/
+Matrix *subtract(Matrix *m1, Matrix *m2)
+{
+	Matrix *result = NULL;
+	
+	// TO DO
+
+	return result;
+}
+
+/****************************************************************************
+ * Creates the transpose matrix of the input matrix and returns a pointer	*
+ * to the result matrix. Use create(), getValueAt(), and setValueAt() 		*
+ * functions to implement this function.									*
+ * DO NOT modify the input matrix.											*
+ ***************************************************************************/
+Matrix *transpose(Matrix *m)
+{
+	Matrix *result = NULL;
+	
+	// TO DO
+
+	return result;
+}
+
+/****************************************************************************
+ * Creates a matrix that is the product of the given scalar value and		*
+ * the input matrix and returns a pointer to the result matrix.				*
+ * Use create(), getValueAt(), and setValueAt() functions to implement 		*
+ * this function.															*
+ * DO NOT modify the input matrix.											*
+ ***************************************************************************/
+Matrix *scalarMultiply(Matrix *m, int scalar)
+{
+	Matrix *result = NULL;
+	
+	// TO DO
+
+	return result;
+}
+
+/****************************************************************************
+ * If the input matrices are compatible, then multiplies the input matrices	*
+ * and returns a pointer to the result matrix. Use create(), getValueAt(), 	*
+ * and setValueAt() functions to implement this function.					*														*
+ *																			*
+ * If the input matrices are not compatible, return NULL.					*
+ * DO NOT modify the input matrices.										*											*
+ ***************************************************************************/
+Matrix *multiply(Matrix *m1, Matrix *m2)
+{
+	Matrix *result = NULL;
+	
+	// TO DO
+
+	return result;
+}
+
+
+
